f9e6d55175665dfb5226e8348fca57acd0a180d0
Romel-Lou22 <romelalba@outlook.es>
2024-10-29 14:32:45 -0500

    Agregado la carpeta Vista Modelo

---
+++ VistaModelo/MainViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FontAwesome.Sharp;
+
+namespace SistemaGestion.VistaModelo
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+        public ViewModelBase CurrentChildView { get => _currentChildView; set { _currentChildView = value; OnPropertyChanged(nameof(CurrentChildView)); } }
+        public string Caption { get => _caption; set { _caption = value; OnPropertyChanged(nameof(Caption)); } }
+        public IconChar Icon { get => _icon; set { _icon = value; OnPropertyChanged(nameof(Icon)); } }
+        
+
+        //Command
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowVentasViewCommand { get; }
+        public ICommand ShowProvedoresViewCommand { get; }
+        public ICommand ShowProductosViewModel { get; }
+        public ICommand ShowClientesViewModel { get; }
+        public ICommand ShowStockViewModel { get; }
+        public ICommand ShowFacturasViewModel { get; }
+        public ICommand ShowReporteViewModel { get; }
+        public ICommand ShowFinanzasViewModel { get; }
+        
+
+        public MainViewModel()
+        {
+            //iniciacion comando
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowVentasViewCommand = new ViewModelCommand(ExecuteShowVentasViewCommand);
+            ShowProvedoresViewCommand = new ViewModelCommand(ExecuteShowProvedoresViewCommand);
+            ShowProductosViewModel = new ViewModelCommand(ExecuteShowProductosViewModel);
+            ShowClientesViewModel = new ViewModelCommand(ExecuteShowClientesViewModel);
+            ShowStockViewModel = new ViewModelCommand(ExecuteShowStockViewModel);
+            ShowFacturasViewModel = new ViewModelCommand(ExecuteShowFacturasViewModel);
+            ShowReporteViewModel = new ViewModelCommand(ExecuteShowReporteViewModel);
+            ShowFinanzasViewModel = new ViewModelCommand(ExecuteShowFinanzasViewModel);
+
+
+
+
+            //Vistas por defecto
+            ExecuteShowHomeViewCommand(null);
+
+        }
+
+        private void ExecuteShowFinanzasViewModel(object obj)
+        {
+            CurrentChildView = new FinanzasViewModel();
+            Caption = "Finanzas";
+            Icon = IconChar.SackDollar;
+        }
+
+        private void ExecuteShowReporteViewModel(object obj)
+        {
+            CurrentChildView = new ReportesViewModel();
+            Caption = "Reportes";
+            Icon = IconChar.FileContract;
+        }
+
+        private void ExecuteShowFacturasViewModel(object obj)
+        {
+            CurrentChildView = new FacturasViewModel();
+            Caption = "Facturas";
+            Icon = IconChar.FileInvoiceDollar;
+        }
+
+        private void ExecuteShowStockViewModel(object obj)
+        {
+            CurrentChildView = new StockViewModel();
+            Caption = "Stock";
+            Icon = IconChar.BoxesStacked;
+        }
+
+        private void ExecuteShowClientesViewModel(object obj)
+        {
+            CurrentChildView = new ClientesViewModel();
+            Caption = "Clientes";
+            Icon = IconChar.Users;
+        }
+
+        private void ExecuteShowProductosViewModel(object obj)
+        {
+            CurrentChildView = new ProductosViewModel();
+            Caption = "Productos";
+            Icon = IconChar.ShoppingBag;
+        }
+
+        private void ExecuteShowProvedoresViewCommand(object obj)
+        {
+            CurrentChildView = new ProvedoresViewModel();
+            Caption = "Provedores";
+            Icon = IconChar.TruckFast;
+        }
+
+        private void ExecuteShowVentasViewCommand(object obj)
+        {
+            CurrentChildView = new VentasViewModel();
+            Caption = "Ventas";
+            Icon = IconChar.CartShopping;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Home";
+            Icon = IconChar.Home;
+        }
+    }
+
+
+
+
+}
---
+++ VistaModelo/ViewModelCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SistemaGestion.VistaModelo
+{
+     public class ViewModelCommand : ICommand
+    {
+        //Esta clase tambien se la llama relay command
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExecuteAction;
+        private ICommand executeShowFinanzasViewModel;
+
+        //Constructor
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null;
+        }
+
+        public ViewModelCommand(ICommand executeShowFinanzasViewModel)
+        {
+            this.executeShowFinanzasViewModel = executeShowFinanzasViewModel;
+        }
+
+        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
