1e60ab415c714b9301f8addafd823e94f6779526
Romel-Lou22 <romelalba@outlook.es>
2025-03-09 19:48:49 -0500

    Actualización modulo finanzas

---
+++ Models/Notificacion.cs
@@ -0,0 +1,16 @@
+﻿using SistemaGestion.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestion.Models
+{
+    public class Notificacion : ViewModelBase
+    {
+        public string Mensaje { get; set; }
+        public string Icono { get; set; }
+        public string IconoColor { get; set; }
+    }
+}
--- View/HomeView.xaml.cs
+++ View/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaGestion.VistaModelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace SistemaGestion.View
         public HomeView()
         {
             InitializeComponent();
+           
+            this.DataContext = new HomeViewModel();
         }
     }
 }
